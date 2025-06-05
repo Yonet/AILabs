23965a02a8cdc638222e00d48c4fd49af2d3e145
Yönet <3523671+Yonet@users.noreply.github.com>
2025-06-05 15:24:00 -0700

    (bug): fix build error

---
+++ packages/scraper-core/src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.placeholderFunction = placeholderFunction;
+function placeholderFunction() {
+    return true;
+}
